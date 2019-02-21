3a56cac1ef13aea3e447b6f618f47b21ec090207
Vledder <remi@studiospindle.nl>
2019-02-21 11:24:51 +0100

    👕 prettier fixes

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,20 @@
-
 import React, { Component } from 'react';
 
-import Layout from "../components/Layout";
-import Intro from "../components/Intro";
-import Main from "../components/Main";
-import List from "../components/List";
+import Layout from '../components/Layout';
+import Intro from '../components/Intro';
+import Main from '../components/Main';
+import List from '../components/List';
 
-import connect from "../content/connect";
+import connect from '../content/connect';
 
 class Index extends Component {
-
   render() {
     return (
-        <Layout>
-          <Intro />
-          <Main />
-          <List entries={connect} />
-        </Layout>
+      <Layout>
+        <Intro />
+        <Main />
+        <List entries={connect} />
+      </Layout>
     );
   }
 }
